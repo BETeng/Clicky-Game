31e1936c9da81151cce7212dd37ab9127dc07571
BETeng <brian7.teng@gmail.com>
2019-08-05 13:43:47 -0500

    file tree setup

---
+++ clickygame/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Scoreboard from './Components/Scoreboard';
+import Board from './Components/Board';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <header>
+        <h1>Memory Click Game</h1>
+       
+        </header>
+        
+      </div>
+    );
+  }
+
+
+
+}
+export default App; 
