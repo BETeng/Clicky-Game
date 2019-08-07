4ad6d4b6fd316ee322ccc4f4e9b3caae80e228ce
BETeng <brian7.teng@gmail.com>
2019-08-06 20:00:43 -0500

    scoreboard functional

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -4,14 +4,28 @@ import Board from './Components/Board';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      currentScore: 0,
+      record: 0
+    }
+  }
+
+
   render() {
     return (
       <div>
         <header>
-        <h1>Memory Click Game</h1>
-       
+          <h1>Memory Click Game</h1>
+          <Scoreboard
+            score={this.state.currentScore}
+            record={this.state.record}
+          />
+          <h2>Click each image only once, if a duplicate is clicked, you lose!!</h2>
+
         </header>
-        
+
       </div>
     );
   }
