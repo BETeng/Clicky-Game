4026f81c86bb9f157c725b089a0489e3231a18c9
BETeng <brian7.teng@gmail.com>
2019-08-11 20:48:42 -0500

    added cardjs and boardjs

--- clickygame/src/Components/Board.js
+++ clickygame/src/Components/Board.js
@@ -1,4 +1,38 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import pokemon from '../data/pokemon.json';
+import Card from './Card';
+
+class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemon: pokemon,
+      clickedPokemon: []
+    };
+  };
+
+render(){
+  let eachPokemon = this.state.pokemon.map(
+    pokemon => 
+    <Card
+      key ={pokemon.id}
+      id={pokemon.id}
+      image={pokemon.image}
+      />
+  )
+  return (
+    <div id="board" className={this.props.shake}>
+        {eachPokemon}
+    </div>
+);
+}
 
 
+}
+
+// Board.propTypes = {
+  
+// }
+
+export default Board;
