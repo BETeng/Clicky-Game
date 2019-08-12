b0137bb4717e64d25244bdb595e1a7a10e7e54be
BETeng <brian7.teng@gmail.com>
2019-08-11 23:46:49 -0500

    set up file tree

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,45 +1,39 @@
-import React, { Component } from 'react';
-import Scoreboard from './Components/Scoreboard';
-import Board from './Components/Board';
+import React from "react";
+// import MemoryTile from "./components/MemoryTile";
+// import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import tiles from "./tiles.json";
+import "./App.css";
 
-class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      currentScore: 0,
-      record: 0
-    }
-  }
-  handleScoreChange = () => {
-    this.setState({
-      currentScore: this.state.currentScore+1
-    })
-  }
-
+class App extends React.Component {
+  state = {
+    tiles,
+    score: 0,
+    message: "Click any tile to start!"
+  };
 
   render() {
     return (
-      <div>
-        <header>
-          <h1>Memory Click Game</h1>
-          <Scoreboard
-            score={this.state.currentScore}
-            record={this.state.record}
-          />
-          <h2>Click each image only once, if a duplicate is clicked, you lose!!</h2>
-        </header>
-        <section>
-          <Board
-            scoreChange={this.handleScoreChange}
-          />
-        </section>
-
-      </div>
+      <>
+        <div className="top">
+          <h1 className="title">Pokemon Memory Game</h1>
+          <Score score={this.state.score} message={this.state.message} />
+        </div>
+        <Wrapper>
+          {this.state.tiles.map(pokemon => {
+            return (
+              <MemoryTile
+                image={pokemon.image}
+                key={pokemon.id}
+                id={pokemon.id}
+                clickHandler={this.clickHandler}
+              />
+            );
+          })}
+        </Wrapper>
+      </>
     );
   }
-
-
-
 }
-export default App; 
+
+export default App;
